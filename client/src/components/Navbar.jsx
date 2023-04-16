3295d0f60e3dbf4e73b7893d7faf2352f1a61846
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-04-17 02:50:02 +0530

    Tailwind not hooked properly , images are properly

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+// THIS IS THE EXACT NAVABR FILE USED IN FINAL FOLDER
+
 import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/images/logo.png";
+import logo from "../../images/logo.png";
 
 const NavBarItem = ({ title, classprops }) => (
   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
@@ -13,6 +15,8 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
+
+
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
